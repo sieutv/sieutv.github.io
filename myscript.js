2bd039134f3df4e3b2c507d16bb53a31ccf6fe36
Your Name <you@example.com>
2016-09-16 16:00:53 +0700

    add

---
+++ myscript.js
@@ -0,0 +1,5 @@
+document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>');
+document.write('<script src="//cdn.firebase.com/js/client/2.4.2/firebase.js"></script>');
+document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.0/moment.min.js"></script>');
+document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.1.0/js/md5.js"></script>');
+document.write('<script src="//vjs.zencdn.net/5.11.6/video.js"></script>');
